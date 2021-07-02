e36ac636ce397afa9e99fb6a9a69fa39a595224f
Arjun Ladha <70059376+TigerNinja22@users.noreply.github.com>
2021-07-02 20:00:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import apis.employee
+from apis.employee import *
+import os
+
+os.chdir(r'C:\Users\arjun\Documents\Arjun\coding\python work learning\sqlite3\employee manager')
+
+option = input("Enter \"add\" in the terminal to add a new emplyee into the db.\nEnter \"close\" in the terminal to close.\nYour option : ")
+
+if option == 'add':
+    self = None
+    first = input('Input first name of employee : ')
+    last = input('Input last name of employee : ')
+    salary = input('Input the salary of employee : ')
+    Employee.add_emp(self, first, last, int(salary))
+    option = input("Enter \"add\" in the terminal to add a new emplyee into the db.")
+if option == 'close':
+    self = None
+    print('Closing...')
+    Employee.close(self)
+    print('Closed')
+else:
+    print("That's not a valid option!")
+    option = input("Enter \"add\" in the terminal to add a new emplyee into the db.")
